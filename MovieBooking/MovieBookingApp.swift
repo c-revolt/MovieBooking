f6a1018977d038bd4ee55dee3b4509f8347412b0
Alexander <a.pride.xxi@tutanota.com>
2023-08-23 13:25:43 +0300

    First Commit

---
+++ MovieBooking/MovieBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookingApp.swift
+//  MovieBooking
+//
+//  Created by Александр Прайд on 23.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
